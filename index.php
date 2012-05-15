df10f7172850879b4f4588537ef346cb2db5eb1b
Daniel Nilsson <danielenilsson@gmail.com>
2012-05-15 14:36:22 +0200

    index och css

---
+++ index.php
@@ -0,0 +1,18 @@
+<html>
+	<head>
+		<title>
+			MSD - Project
+		</title>
+	</head>
+
+	<body>
+		<div class="list">
+			Hello
+		</div>
+		
+		<div class="search">
+			World
+		</div>
+	</body>
+	
+</html>
