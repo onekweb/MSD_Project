52f3d29cbf478f52f6970dcd5cee9998a866b11a
Mario Onekanda <mario.onekanda@hotmail.com>
2012-05-15 16:12:26 +0200

    php-functions

--- public/includes/function/allfunctions.php
+++ public/includes/function/allfunctions.php
@@ -1,4 +1,7 @@
 <?php // allfunctions.php
 
+function database()
+{ 
+}
 
 ?>
--- public/includes/search.php
+++ public/includes/search.php
@@ -1,4 +1,59 @@
 <?php // search.php
-
-
+interface Isearch
+{
+	public function connection();
+	public function dexonnection();
+	public function users();
+	public function showAartist();
+	public function showAlbum();
+	public function showGenre();
+	public function showSong();
+	public function showPlayList();
+	
+	
+}
+class Search implements Isearch
+{
+	public function __construct()
+	{
+		
+	}
+	public function connection()
+	{
+		
+	}
+	
+	public function dexonnection()
+	{
+		
+	}
+	
+	public function users()
+	{
+		
+	}
+	
+	public function showAartist()
+	{
+		
+	}
+	public function showAlbum()
+	{
+		
+	}
+	
+	public function showGenre()
+	{
+		
+	}
+	public function showSong()
+	{
+		
+	}
+	public function showPlayList()
+	{
+		
+	}
+}
+}
 ?>
