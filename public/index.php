80ac2931b9c65415cc6055de4cedb0df18912ce4
Daniel Nilsson <danielenilsson@gmail.com>
2012-05-15 15:32:33 +0200

    index and css update

--- public/index.php
+++ public/index.php
@@ -1,17 +1,23 @@
 <html>
 	<head>
+		<meta charset=UTF-8>
 		<title>
 			MSD - Project
 		</title>
+		<link rel="stylesheet" type="text/css" href="./css/style.css">
 	</head>
 
 	<body>
-		<div class="list">
-			Hello
-		</div>
+		<div class="main">
+			
+			<div class="list">
+				Hello
+			</div>
 		
-		<div class="search">
-			World
+			<div class="search">
+				<input type="text" placeholder="Sök..">
+				<input type="submit" value="Sök">
+			</div>
 		</div>
 	</body>
 	
