db31db0244a1f04911e97de162e7319ed03608ce
Mario Onekanda <mario.onekanda@hotmail.com>
2012-05-21 16:59:28 +0200

    modifieded seach function in php

--- public/includes/search.php
+++ public/includes/search.php
@@ -1,4 +1,6 @@
 <?php // search.php
+require ('function/allfunctions.php');
+require ('connection.php');
 interface Isearch
 {
 	public function connection();
@@ -14,8 +16,25 @@ interface Isearch
 }
 class Search implements Isearch
 {
+	public $search;
 	public function __construct()
 	{
+	 global $connection, $db_select;
+	 $this->connection = $connection;
+	 $this->db_select = $db_select;
+	 $this->search =  $_POST['search'];	
+	}
+	public function search()
+	{
+		$this-> search = $_POST['search'];
+		if(isset($this->search))
+		{
+			$this->connection;
+			$this->db_select;
+			$sql = mysql_query("SELECT * FROM $this->db_select WHERE artists, album, songs LIKE % $this->search%");
+			$result = mysql_fetch_row($sql);
+			echo "There is ".$result;
+		}
 		
 	}
 	public function connection()
@@ -55,5 +74,7 @@ class Search implements Isearch
 		
 	}
 }
-}
+$newSearch = new Search();
+$newSearch->search();
+
 ?>
