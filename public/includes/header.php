a78b6ffcd8ed7c9f56e7578526e36d7236f38073
Mario Onekanda <mario.onekanda@hotmail.com>
2012-05-23 11:55:14 +0200

    removed search

--- public/includes/header.php
+++ public/includes/header.php
@@ -1,3 +1,8 @@
-<?php
-	echo"Hello";
-?>
+<html>
+	<head>
+		<meta charset=UTF-8/>
+		<title>
+			MSD - Project
+		</title>
+		<link rel="stylesheet" type="text/css" href="./css/style.css">
+	</head>
