14a6ebd80ce14c1e515ec7c77c7bff07bf3a7ebe
Mario Onekanda <mario.onekanda@hotmail.com>
2012-05-23 11:59:55 +0200

    update for the whole page

--- public/includes/header.php
+++ public/includes/header.php
@@ -1,8 +1,3 @@
-<html>
-	<head>
-		<meta charset=UTF-8/>
-		<title>
-			MSD - Project
-		</title>
-		<link rel="stylesheet" type="text/css" href="./css/style.css">
-	</head>
+<?php
+	echo"Hello";
+?>
