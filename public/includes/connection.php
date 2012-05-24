d5a18bbb39b6dfb1bfc5607f1d98cbbc37e0446a
Mario Onekanda <mario.onekanda@hotmail.com>
2012-05-24 10:56:19 +0200

    modified search function

--- public/includes/connection.php
+++ public/includes/connection.php
@@ -1,30 +1,6 @@
 <?php //connection.php
 require('constants.php');
 
-$connection = new mysqli('localhost', 'root', '', 'msd_project') or die('error');
+$connection= new mysqli(DB_SERVER, DB_USER, DB_PASS, DB_NAME) or die('error');
 
-
-
-
-
-
-/*
-
-// 1 Create a database connection
-$connection = mysql_connect(DB_SERVER, DB_USER, DB_PASS);
-if(!$connection)
-{
-	die("Connection failed" .mysql_error());
-}
-
-
-// 2 Select a dabatase
-$db_select = mysql_select_db(DB_NAME);
-if(!$db_select)
-{
-	die("Failed to select a database ". mysql_error());
-}
- * 
- * /
- */
 ?>
