38c0ade0e1a7917a2ac10b8089bc2e14267b048a
lubwimi <lubwimi@hotmail.com>
2012-06-01 13:10:10 +0200

    Fixed genres in search

--- public/includes/functions.php
+++ public/includes/functions.php
@@ -46,80 +46,83 @@ interface Isearch  // Order all the functions
      { 
 		if(isset($_POST['find']))
 		{
-				$this->find = $_POST['find']; // the post variable
-				if($this->find !== '')//if the variable is not empety, exectued the code bellow
-				{ 
-		            $mysqli = $this->connection;
+			$this->find = $_POST['find']; // the post variable
+			if($this->find !== '')//if the variable is not empety, exectued the code bellow
+			{
+			$mysqli = $this->connection;
+			
+			$query = "SELECT songs.song as Song, songs.id , albums.album as Album, artists.artist as Artist, genres.genre as Genre FROM songs
+						LEFT JOIN albums ON albums.id = songs.albums_id
+						LEFT JOIN artists ON artists.id = albums.artists_id
+						LEFT JOIN genres ON genres.id = artists.genres_id
+						WHERE song LIKE '%".$this->find."%'
+						OR album LIKE '%".$this->find."%'
+						OR artist LIKE '%".$this->find."%'
+						OR genre LIKE '%".$this->find."%'";
+			
+			
+			
+			$queryPlaylistName = "SELECT name FROM playlists";
+			$queryartist = NULL;
+			$stmt1 = $mysqli->prepare($query);
+			$stmt1->bind_result($song, $id, $album, $artist, $genre);
+			$stmt1->execute();
+			$songs = array();
+			$i = 0;
+			while($stmt1->fetch()){
+				if ($artist == NULL || $album == NULL ||$song == NULL ||$genre == NULL) {
+			
+					echo"Could not find the artist or the song your searched!<br />";
+					echo"Please try again!<br />";
+				}
+				$songs[$i] = array();
+				$songs[$i]['song'] = $song;
+				$songs[$i]['artist'] = $artist;
+				$songs[$i]['album'] = $album;
+				$songs[$i]['id'] = $id;
+				$songs[$i]['genre'] = $genre;
+				$i++;
+			}
+			
+			$stmt1->close();
+			$stmt2 = $mysqli->prepare($queryPlaylistName);
+			$stmt2->bind_result($playlistNames);
+			$stmt2->execute();
+			$playlists = array();
+			while($stmt2->fetch()){
+			$playlists[]=$playlistNames;
+			}
+			
+			echo "<table border='1'>";
+			echo "<th>Song Id</th><th>Songs</th><th>Albums</th><th>Artists</th><th>Genre</th>";
+			echo "<form action='./includes/functions.php' method='post'>
+			<input type='text' name='songId' placeholder='Song Id' />
+			<select name='playlists'>";
+			foreach($playlists as $playlistName){
+				echo "<option>".$playlistName."</option>";
+			}
+			echo " </select>
+			<input type='submit' name='playlist' value='Add song' />
+			</form>";
+			foreach($songs as $song){
+				echo "<tr><td>".$song['id']."</td><td>".$song['song']."</td><td>" .$song['album']."</td><td>".$song['artist']."</td><td>".$song['genre']."</tr>";
+			
+			// Kvar att göra, en for loop som skriver ut all spellistor som finns.
+			
+			}
+			echo "</table>";
+			
+			}
+			elseif($this->find == '')
+			{
+				echo"Opps, you have to write something on the searchfield.<br />";
+				echo"Please try again!<br />";
+			}
+			}
 
-		            $query = "SELECT songs.song as Song, songs.id , albums.album as Album, artists.artist as Artist FROM songs
-		                        LEFT JOIN albums ON albums.id = songs.albums_id
-                                LEFT JOIN artists ON artists.id = albums.artists_id
-		                        WHERE song LIKE '%".$this->find."%'
-		                        OR album LIKE '%".$this->find."%'
-								OR artist LIKE '%".$this->find."%'";
-								
 
-					
-					$queryPlaylistName = "SELECT name FROM playlists";
-					$queryartist = NULL;					
-		            $stmt1 = $mysqli->prepare($query);
-		            $stmt1->bind_result($song, $id, $album, $artist);
-		            $stmt1->execute();
-					$songs = array();
-					$i = 0;
-					while($stmt1->fetch()){
-			            if ($artist == NULL || $album == NULL ||$song == NULL) 
-			            {
-	
-			                echo"Could not find the artist or the song your searched!<br />";     
-			                echo"Please try again!<br />";            
-			            }
-						$songs[$i] = array();
-						$songs[$i]['song'] = $song;
-						$songs[$i]['artist'] = $artist;
-						$songs[$i]['album'] = $album;
-						$songs[$i]['id'] = $id;
-						$i++;
-					}
-					$stmt1->close();
-					$stmt2 = $mysqli->prepare($queryPlaylistName);
-					$stmt2->bind_result($playlistNames);
-					$stmt2->execute();
-					$playlists = array();
-					while($stmt2->fetch()){ 
-						$playlists[]=$playlistNames;
-					}
-					
-					echo "<table border='1'>";
-		            echo "<th>Song Id</th><th>Songs</th><th>Albums</th><th>Artists</th>";
-					echo "<form action='./includes/functions.php' method='post'>
-							<input type='text' name='songId' placeholder='Song Id' />
-							<select name='playlists'>";
-								foreach($playlists as $playlistName){ 
-									 echo "<option>".$playlistName."</option>";
-								 }
-					echo "	</select>
-							<input type='submit' name='playlist' value='Add song' />
-						  </form>";
-		            foreach($songs as $song){
-		                echo "<tr><td>".$song['id']."</td><td>".$song['song']."</td><td>" .$song['album']."</td><td>".$song['artist']."</td></tr>";
-						
-						// Kvar att göra, en for loop som skriver ut all spellistor som finns.
-						
-					}
-		            echo "</table>";  
-
-		           } 
-		          elseif($this->find == '')
-		          {
-		           	 echo"Opps, you have to write something on the searchfield.<br />";     
-		             echo"Please try again!<br />";  
-		          }
-				  
-				  
-
-	      }
-     }
+		}
+     
    
     public function connection() //Functions for the connection to the server and the database
     {
